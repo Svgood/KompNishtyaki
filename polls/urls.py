4552441e232197d506e1aeec36c3edd371ce51b2
Svgood <n.bilyk22@gmail.com>
2018-05-22 23:46:29 +0300

    modified

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('goods/', views.goods, name='goods'),
+    path('goods/<int:good_id>', views.good, name='good'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote', views.vote, name='vote'),
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Question
 from .models import *
 
@@ -13,8 +14,20 @@ def index(request):
 
 def goods(request):
     goods_to_show = Good.objects.all()
-    context = {'goods' : goods_to_show}
+    cat = None
+    if request.method == "POST":
+        cat = request.POST['choice']
+    if cat is not None and cat != "Все":
+        goods_to_show = Good.objects.filter(category__category_name=cat)
+    cats = Category.objects.all()
+    context = {'goods' : goods_to_show,
+               'cats' : cats}
     return render(request, 'goods.html', context)
+
+def good(request, good_id):
+    item = Good.objects.filter(id=int(good_id)).get()
+    context = { 'good' : item }
+    return render(request, 'good.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question {}".format(question_id))
